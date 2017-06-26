69f215f8b044dedcab0194530cfc2e4ef94dac0d
Chris Mason <meysuhn@gmail.com>
2017-06-26 20:29:12 +0100

    Time feature finally working
    Minor issue in that the time is slightly wrong, but other than that…!

--- csv.js
+++ csv.js
@@ -11,16 +11,35 @@ var fileName = `data/${date}.csv`; // Template literal to create file name with 
 
 ////////////////////////////////////////////////////
 // The below code works via the .JSON file
-const shirtJSON = require('./results.json'); // store the JSON file as a variable
-let shirtData = shirtJSON.shirts; // This is from the ./results/json file. Shirts is the array name.
+//const shirtJSON = require('./results.json'); // store the JSON file as a variable
+//let shirtData = shirtJSON.shirts; // This is from the ./results/json file. Shirts is the array name.
 ///////////////////////////////////////////////////
 
+//const shirtJSON = require('./scraper4.js'); // store the JSON file as a variable
+//let shirtData = obj.shirts;
 
-let result = json2csv({ data: shirtData, fields: fields, fieldNames: fieldNames }); // run JSON2CSV
+//let result = json2csv({ data: obj, fields: fields, fieldNames: fieldNames }); // run JSON2CSV
 //console.log(result);
 
-function makeCSV () {
-  //console.log("makeCSV has run");
+
+function makeCSV (obj) {
+  let shirtData = obj.shirts;
+  let result = json2csv({ data: shirtData, fields: fields, fieldNames: fieldNames }); // run JSON2CSV
+  //let shirtData = obj.shirts;
+  console.log(obj); // This is working. The object is getting to this point.
+  var shirtsObj = obj;
+try {
+  //let result = json2csv({ data: shirtsObj, fields: fields, fieldNames: fieldNames }); // run JSON2CSV
+  console.log(result);
+} catch (err) {
+// Errors are thrown for bad options, or if the data is empty and no fields are provided.
+// Be sure to provide fields if it is possible that your data array will be empty.
+console.error(err);
+}
+
+
+
+  console.log("makeCSV has run");
   fs.writeFile(fileName, result, function(err) { //save file to data dir
     console.log('file saved');
     if (err) throw err;
